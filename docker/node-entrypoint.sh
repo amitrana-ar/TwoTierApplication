d0c6cfd4283d9c6d19d21bb725b10223f163b6ba
Amit Rana <ar626991@gmail.com>
2025-09-10 12:29:23 +0000

    node yml file changed

--- docker/node-entrypoint.sh
+++ docker/node-entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 set -e
 
-yarn install
+# Only build once, not on every container start
+if [ ! -d "/var/www/node_modules" ]; then
+  yarn install
+fi
+
 yarn build
-yarn dev
+
+# Start dev server on all interfaces (0.0.0.0) so ECS can access it
+yarn dev --host 0.0.0.0
